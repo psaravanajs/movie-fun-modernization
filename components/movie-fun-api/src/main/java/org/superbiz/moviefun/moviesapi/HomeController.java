7e6acf46b1225095cd83dad254eee146c191ec1c
rohanrajput009 <rohan_rajput@dell.com>
2019-04-25 14:13:53 +0530

    microservices

---
+++ components/movie-fun-api/src/main/java/org/superbiz/moviefun/moviesapi/HomeController.java
@@ -0,0 +1,49 @@
+package org.superbiz.moviefun.moviesapi;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.superbiz.moviefun.moviesapi.albumsapi.AlbumFixtures;
+import org.superbiz.moviefun.moviesapi.albumsapi.AlbumInfo;
+import org.superbiz.moviefun.moviesapi.albumsapi.AlbumsClient;
+import org.superbiz.moviefun.moviesapi.moviesapi.MovieFixtures;
+import org.superbiz.moviefun.moviesapi.moviesapi.MovieInfo;
+import org.superbiz.moviefun.moviesapi.moviesapi.MoviesClient;
+
+import java.util.Map;
+
+@Controller
+public class HomeController {
+
+    private final MoviesClient moviesClient;
+    private final AlbumsClient albumsClient;
+    private final MovieFixtures movieFixtures;
+    private final AlbumFixtures albumFixtures;
+
+    public HomeController(MoviesClient moviesClient, AlbumsClient albumsClient, MovieFixtures movieFixtures, AlbumFixtures albumFixtures) {
+        this.moviesClient = moviesClient;
+        this.albumsClient = albumsClient;
+        this.movieFixtures = movieFixtures;
+        this.albumFixtures = albumFixtures;
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/setup")
+    public String setup(Map<String, Object> model) {
+        for (MovieInfo movie : movieFixtures.load()) {
+            moviesClient.addMovie(movie);
+        }
+
+        for (AlbumInfo albumInfo : albumFixtures.load()) {
+            albumsClient.addAlbum(albumInfo);
+        }
+
+        model.put("movies", moviesClient.getMovies());
+        model.put("albums", albumsClient.getAlbums());
+
+        return "setup";
+    }
+}
